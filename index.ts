3e323d2a6fe8903ffc915e123e8efc61ef7eef63
cesar05 <cesar920528@gmail.com>
2020-11-09 21:14:04 -0500

    Se declara una seríe de operadores los cuales se subscriben implicitamente en el observable

--- index.ts
+++ index.ts
@@ -1,35 +1,12 @@
-import { Observable, Observer } from "rxjs";
-import { map, filter } from "rxjs/operators";
+import { of } from "rxjs";
+import { map, filter, delay, tap, scan } from "rxjs/operators";
 
-const myObservable = new Observable(subscriber => {
-  subscriber.next("holis"),
-    subscriber.next(10),
-    subscriber.next(20),
-    subscriber.error("Holis soy un error jijii");
-});
-
-const myObservable2 = new Observable(subscriber => {
-  subscriber.complete();
-});
-
-//El Pipe es inmutable
-//Usarlo para hacer cosas genericas
-//Se ejecuta antes de enviarle el dato al subscribe
-const myPipe = myObservable.pipe(
-  filter((r: any) => !isNaN(r)),
-  map((r: any) => {
-    return r + 10;
-  })
+const source = of("World").pipe(
+  map(x => `Hello ${x}!`),
+  tap(x => console.log(x)),
+  delay(2000),
+  scan((acc, one) => acc + one, "Holis "),
+  filter(x => x.includes("Holis"))
 );
 
-//No es es inmutable
-//Usarlo para hacer cosas especificas
-const myObserver: Observer<any> = {
-  next: x => {
-    console.log(x);
-  },
-  error: err => console.error(`Ahh te equivocaste`, err),
-  complete: () => console.log("mui trabajo aqui esta realizado")
-};
-
-myPipe.subscribe(myObserver);
+source.subscribe(console.log);
